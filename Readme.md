bec478ca7dc2c3677bebf1aad6d612825b3e8578
Fabian Marcus <apps@fabian-marcus.de>
2019-08-15 13:31:36 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,79 @@
+# Phoenix Reisen API Modul
+
+Wiederverwendbarer Stuff zum vereinfachten Handling reaktiver Formulare - instant Validierung für verschiedene Feldtypen, Validierung für gesamtes Formular, Prüfung auf Browser-Support für Datepicker, und anderes.
+
+## Installation
+
+```bash
+npm install --save @phoenixreisen/form
+```
+
+## Anwendung (Beispiel)
+
+Bestimmte Felder importieren
+
+```js
+import { text, int, email, bookingnr } from '@phoenixreisen/form';
+```
+
+Alle Felder importieren
+
+```js
+import fields from '@phoenixreisen/form';
+```
+
+Funktionen importieren
+
+```js
+import { isValidInput, isDateSupported } from '@phoenixreisen/form';
+```
+
+Formular deklarieren
+(Parameter-Signatur der einzelnen Felder beachten)
+
+```js
+const form = {
+    prename: text(),        // Text
+    surname: text(),        // Text
+    age: int(false),        // optionales Feld, Int Validierung
+    bookingnr: bookingnr()  // Validierung auf gültige Buchungsnr.
+};
+```
+
+```html
+<input 
+    type="text"
+    placeholder="Beispiel..."
+    value={form.surname.value()}
+    oninput={e => form.surname.validate(e.target.value)}
+/>
+
+{form.surname.complaint &&
+    <div class="alert alert--warning">
+        Bitte richtig ausfüllen
+    </div>
+}
+```
+
+Bei Submit alles validieren lassen
+
+```js
+function submit(form) {
+    if(!Form.isValidInput(form)) {
+        // bei invaliden Feldern wurde property "complaint" auf true gesetzt.
+        return;
+    }
+}
+```
+
+
+## Deployment
+
+```bash
+
+npm version [major|minor|patch]     # increase version x.x.x => major.minor.patch
+npm publish                         # upload to npm
+
+hg bzw. git commit package.json package-lock.json -m "(npm) Neue Version"
+hg bzw. git push
+```
---
+++ src/form.js
@@ -0,0 +1,405 @@
+import DateTime from 'date-and-time';
+import Stream from 'mithril/stream';
+
+import isIban from 'iban';
+import isInt from 'validator/lib/isInt';
+import isEmpty from 'validator/lib/isEmpty';
+import isNumeric from 'validator/lib/isNumeric';
+
+const DateConfig = {
+    patterns: {
+        de: 'DD.MM.YYYY',
+        en: 'YYYY-MM-DD',
+    },
+    i18n: {
+        de: {
+            firstDay: 1,
+            previousMonth : 'Vorheriger Month',
+            nextMonth     : 'Nächster Monat',
+            months        : ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
+            weekdays      : ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+            weekdaysShort : ['So','Mo','Di','Mi','Do','Fr','Sa'],
+        },
+    },
+};
+
+const UserTypes = {
+    customer: 'customer',
+    employee: 'employee',
+    phoenix: 'phoenix',
+    agency: 'agency',
+};
+
+const ValidationTypes = {
+    empty: 'empty',
+    invalid: 'invalid',
+    notequal: 'not-equal',
+};
+
+// ---
+// FELDER
+// ---
+
+const text = (required = true, hook = undefined) => {
+    const text = {
+        value: Stream(''),
+        complaint: false,
+        mirror: undefined,
+        required: required,
+        validate: (input) => {
+            const { mirror } = text;
+            if((!input || isEmpty(input)) && text.required) {
+                text.complaint = ValidationTypes.empty;
+            } else if(mirror && (mirror.value() !== input)) {
+                text.complaint = ValidationTypes.notequal;
+            } else {
+                text.complaint = false;
+            }
+            if(hook) hook.call(this, input);
+            text.value(input);
+        },
+    };
+    return text;
+};
+
+const int = (required = true, hook = undefined) => {
+    const int = {
+        value: Stream(null),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            if(!input && input !== 0 && int.required) {
+                int.complaint = ValidationTypes.empty;
+            } else if(input && !isInt(input)) {
+                int.complaint = ValidationTypes.invalid;
+            } else {
+                int.complaint = false;
+            }
+            if(hook) hook.call(this, input);
+            int.value(input);
+        },
+    };
+    return int;
+};
+
+const email = (required = true, hook = undefined) => {
+    const email = {
+        value: Stream(''),
+        complaint: false,
+        mirror: undefined,
+        required: required,
+        validate: (input) => {
+            const { mirror } = email;
+            if((!input || isEmpty(input)) && email.required) {
+                email.complaint = ValidationTypes.empty;
+            } else if(isNumeric(input)) {
+                email.complaint = ValidationTypes.invalid;
+            } else if(mirror && mirror.value() !== input) {
+                email.complaint = ValidationTypes.notequal;
+            } else {
+                email.complaint = false;
+            }
+            if(hook) hook.call(this, input);
+            email.value(input);
+        },
+    };
+    return email;
+};
+
+const date = (required = true, hook = undefined) => {
+    const date = {
+        value: Stream(''),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            const { patterns } = DateConfig;
+            if((!input || isEmpty(input)) && date.required) {
+                date.complaint = ValidationTypes.empty;
+            } else if(!DateTime.isValid(input, patterns.de) || input.length < patterns.de.length) {
+                date.complaint = ValidationTypes.invalid;
+            } else {
+                date.complaint = false;
+            }
+            if(hook) hook.call(this, input);
+            date.value(input);
+        },
+        getDate: () => {
+            const { patterns } = DateConfig;
+            if(DateTime.isValid(date.value(), patterns.de)) {
+                return DateTime.parse(date.value(), patterns.de);
+            } else if(DateTime.isValid(date.value(), patterns.en)) {
+                return DateTime.parse(date.value(), patterns.en);
+            } else {
+                return null;
+            }
+        },
+    };
+    return date;
+};
+
+const nativeDate = (required = true, daterange = undefined, hook = undefined) => {
+    const date = {
+        complaint: '',
+        required: required,
+        value: Stream(undefined),
+        validate: (datestring, range = daterange) => {
+            const { parse, isValid } = DateTime;
+            const { patterns } = DateConfig;
+
+            date.complaint = '';
+            if(!datestring || !datestring.length) {
+                date.complaint = 'empty';
+            }
+            else if((!isValid(datestring, patterns.de) || datestring.length < patterns.de.length)
+            && (!isValid(datestring, patterns.en) || datestring.length < patterns.en.length)) {
+                date.complaint = 'invalid';
+            }
+            if(!date.complaint) {
+                const inputdate = isNaN(parse(datestring, patterns.de))
+                    ? parse(datestring, patterns.en)
+                    : parse(datestring, patterns.de);
+                
+                if(range && (range[0] && (inputdate < range[0]) || (range[1] && inputdate > range[1]))) {
+                    date.complaint = 'out-of-range';
+                }
+            }
+            if(hook) {
+                hook.call(this, datestring);
+            }
+            date.value(datestring);
+        },
+        getDate: () => {
+            const { patterns } = DateConfig;
+            if(DateTime.isValid(date.value(), patterns.de)) {
+                return DateTime.parse(date.value(), patterns.de);
+            } else if(DateTime.isValid(date.value(), patterns.en)) {
+                return DateTime.parse(date.value(), patterns.en);
+            } else {
+                return null;
+            }
+        },
+    };
+    return date;
+};
+
+const time = (required = true) => {
+    const time = {
+        value: Stream(''),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            if((!input || isEmpty(input)) && time.required) {
+                time.complaint = ValidationTypes.empty;
+            } else if(!DateTime.isValid(input, 'hh:mm')) {
+                time.complaint = ValidationTypes.invalid;
+            } else {
+                time.complaint = false;
+            }
+            time.value(input);
+        },
+    };
+    return time;
+};
+
+const gender = (required = true) => {
+    const gender = {
+        value: Stream(''),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            if((!input || isEmpty(input)) && gender.required) {
+                gender.complaint = ValidationTypes.empty;
+            } else if(input.length &&
+                input !== 'maenlich' && 
+                input !== 'weiblich' &&
+                input !== 'divers'
+            ) {
+                gender.complaint = ValidationTypes.invalid;
+            }  else {
+                gender.complaint = false;
+            }
+            gender.value(input);
+        },
+    };
+    return gender;
+};
+
+const phone = (required = true) => {
+    const phone = {
+        value: Stream(''),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            if(input.length && !input.match(/^[0-9+-]+$/)) {
+                return; // wenn es keine Ziffer ist, verwerfe es.
+            } else if((!input || isEmpty(input)) && phone.required) {
+                phone.complaint = true;
+            } else {
+                phone.complaint = false;
+            }
+            phone.value(input);
+        },
+    };
+    return phone;
+};
+
+const radio = (required = true, hook = undefined) => {
+    const radio = {
+        value: Stream(null),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            radio.complaint = (input === null && radio.required);
+            if(hook) hook.call(this, input);
+            radio.value(input);
+        },
+    };
+    return radio;
+};
+
+const checkbox = (required = true, hook = undefined) => {
+    const checkbox = {
+        value: Stream(false),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            const checked = input ? true : false;
+            checkbox.complaint = (!checked && checkbox.required);
+            if(hook) hook.call(this, input);
+            checkbox.value(checked);
+        },
+    };
+    return checkbox;
+};
+
+const bookingnr = (required = true, hook = undefined) => {
+    const bookingnr = {
+        value: Stream(''),
+        invalid: false,
+        required: required,
+        validate: input => {
+            bookingnr.invalid = (
+                !input
+                || isEmpty(input) 
+                || !isInt(input)
+                || (input.length !== 6)
+            );
+            if(hook) hook.call(this, input);
+            bookingnr.value(input);
+        },
+    };
+    return bookingnr;
+};
+
+const agencyid = (required = true) => {
+    const agencyid = {
+        value: Stream(''),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            agencyid.complaint = (
+                !input
+                || !isInt(input) 
+                || isEmpty(input) 
+                || input.length !== 6
+            );
+            agencyid.value(input);
+        },
+    };
+    return agencyid;
+};
+
+const iban = (required = true, hook = undefined) => {
+    const iban = {
+        value: Stream(''),
+        complaint: false,
+        required: required,
+        validate: (input) => {
+            iban.complaint = false;
+            if(iban.required && (!input || isEmpty(input))) {
+                iban.complaint = ValidationTypes.empty;
+            }
+            else if(input && !isIban.isValid(input)) {
+                iban.complaint = ValidationTypes.invalid;
+            }
+            if(hook) hook.call(this, input);
+            iban.value(input);
+        },
+    };
+    return iban;
+};
+
+const fields = {
+    text, int,
+    date, time, 
+    radio, checkbox,
+    phone, gender, email,
+    agencyid, bookingnr,
+};
+
+
+// ---
+// FUNKTIONEN
+// ---
+
+/**
+  * gibt die jeweilige Status-CSS-Klasse
+  * für den Submit-Button zurück.
+  * @param {bool} success 
+  * @param {bool/object} error 
+  * @returns {string} CSS Klasse
+  */
+function getBtnStatusClass(success, error) {
+    let classname = '';
+    classname = success ? 'btn--success' : classname;
+    classname = error ? 'btn--danger' : classname;
+    return classname;
+}
+
+/**
+  * Ruf die validate()-Funktionen aller Felder
+  * auf und prüft anschließend, ob alle valide sind.
+  * Gut, um ein ganzes Formular vor Submit zu prüfen.
+  * @param {array} fields 
+  * @returns {bool} ja/nein
+  */
+function isValidInput(fields) {
+    const fieldnames = Object.keys(fields);
+    let hasErrors = false;
+    // validate() aller Felder aufrufen und complaint prüfen
+    for(let i=0; i < fieldnames.length; i+=1) {
+        const field = fields[fieldnames[i]];
+        field.validate(field.value());
+        hasErrors = (field.complaint || hasErrors);
+    }
+    return !hasErrors;
+}
+
+/**
+ * Prüft, ob der Browser einen eigenen
+ * nativen Datepicker mitbringt oder nicht.
+ * @returns {bool} ja/nein
+ */
+function isDateSupported() {
+    const value = 'test';
+    const input = document.createElement('input');
+    input.setAttribute('type', 'date');
+    input.setAttribute('value', value);
+    return (input.value !== value);
+}
+
+
+export {
+    fields,
+    UserTypes,
+    DateConfig,
+    isValidInput,
+    isDateSupported,
+    getBtnStatusClass,
+    ValidationTypes,
+    bookingnr, agencyid,
+    gender, phone, email,
+    text, int, date, nativeDate,
+    time, radio, checkbox, iban,
+};
+export default fields;
